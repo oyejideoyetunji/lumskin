5bcb27b1fc2c7a0ab513124e7ff871542ca010c1
oluwatobi oyejide <moyejide2016@gmail.com>
2021-07-18 22:18:34 +0100

    cosmetic updates

--- src/screens/products/index.tsx
+++ src/screens/products/index.tsx
@@ -19,7 +19,6 @@ const Products: FC = () => {
         cart,
         showCart,
         currency,
-        currentPersonalDetails,
         setCurrentPersonalDetails,
         setShowCart,
         setCart
@@ -126,12 +125,12 @@ const Products: FC = () => {
             : [{ id: item.id, personalDetails: option, count: 1 }, ...cart]
 
         setStoreData<ICart>(StoreKey.CART, newCart)
-        setStoreData<IPersonalizationDetails[]>(
-            StoreKey.PERSONAL_DETAILS, option.length ? option : currentPersonalDetails
-        )
         setCart && setCart(newCart)
-        setCurrentPersonalDetails && setCurrentPersonalDetails(
-            option.length ? option : currentPersonalDetails
+        option.length && setStoreData<IPersonalizationDetails[]>(
+            StoreKey.PERSONAL_DETAILS, option
+        )
+        option.length && setCurrentPersonalDetails && setCurrentPersonalDetails(
+            option
         )
     }
 
